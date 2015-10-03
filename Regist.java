f0a74834de2f33135993e5b9f02408960dd2ed89
Saterblue <Saterblue@users.noreply.github.com>
2015-10-03 10:52:56 +0800

    Update Regist.java

--- Regist.java
+++ Regist.java
@@ -4,16 +4,15 @@ public class Regist {
 	private static String password;
 	private static String repasswd;
 	private Regist(){}
-	
-	//注册功能
+	//注册功能,注册后返回到登陆界面
 	public static void start() {
 		System.out.println("请输入您要注册的用户名：");
 		name=Tools.getInput();
 		index=UserList.searchList(name);
 		if(index>-1) {
-			System.out.println("用户已存在！请重新进行注册！");
+			System.out.println("用户已存在！注册失败，返回开始菜单...！");
 			Tools.showLine();
-			start();
+			StartMenu.start();
 		}else{
 			System.out.println("请输入您要的密码");
 			password=Tools.getInput();
@@ -23,12 +22,12 @@ public class Regist {
 				UserList.addUser(-index, name, password);
 				System.out.println("注册成功！返回开始菜单...");
 				Tools.showLine();
-				UserChoose.show();
+				StartMenu.start();
 			}
 			else {
 				System.out.println("密码不一致，注册失败！返回开始菜单...");
 				Tools.showLine();
-				UserChoose.show();
+				StartMenu.start();
 			}
 		}
 	}
